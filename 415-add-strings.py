3fe351f065b481caa02ed02dbcd02477507f952c
liangpinlk <liangpinglk@gmail.com>
2020-08-21 11:24:45 +0800

    perf:415 add strings. 优化

--- 415-add-strings.py
+++ 415-add-strings.py
@@ -1,6 +1,6 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        result = []
+        result = ''
         num1_len = len(num1) - 1
         num2_len = len(num2) - 1
         index_diff = num1_len - num2_len
@@ -10,27 +10,24 @@ class Solution:
             if j < 0:
                 if other_add == 1:
                     tmp = int(num1[i]) + other_add
-                    result.append(str(tmp % 10))
+                    result = str(tmp % 10) + result
                     other_add = 1 if tmp >= 10 else 0
                 else:
-                    result.append(num1[i])
+                    result = num1[i] + result
             else:
                 tmp = int(num1[i]) + int(num2[j]) + other_add
-                result.append(str(tmp % 10))
+                result = str(tmp % 10) + result
                 other_add = 1 if tmp >= 10 else 0
         for i in range(- index_diff - 1, -1, -1):
             if other_add == 1:
                 tmp = int(num2[i]) + other_add
-                result.append(str(tmp % 10))
+                result = str(tmp % 10) + result
                 other_add = 1 if tmp >= 10 else 0
             else:
-                result.append(num2[i])
+                result = num2[i] + result
         if other_add == 1:
-            result.append(str(other_add))
-        final_result = ''
-        for i in range(len(result) - 1, -1, -1):
-            final_result += result[i]
-        return final_result
+            result = str(other_add) + result
+        return result
 
 
 if __name__ == '__main__':
